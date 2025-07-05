77ac3e16d08af87abec94e21b2100a39cac1bc6c
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-07-05 12:35:01 -0400

    Refactor kernel_loader.asm to use 32-bit mode and properly define Multiboot header

--- kernel_loader.asm
+++ kernel_loader.asm
@@ -1,10 +1,12 @@
-bits 64
- dd 0x1BADB002
- dd 0x0
- dd - 0x1BADB002
-
+bits 32               ; Multiboot expects 32-bit mode at entry
 extern _kmain
- start:
-  cli
-  call _kmain
-  jmp $
+
+; Multiboot header (must be within first 8 KB and 4-byte aligned)
+dd 0x1BADB002         ; magic
+dd 0x00000000         ; flags
+dd 0xE4524FFE         ; checksum = -(magic + flags)
+
+start:
+    cli
+    call _kmain
+    jmp $
